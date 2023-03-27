82dee9967113893eb6f1a7961ccad8a13d3988b4
BS-Eg <120326341+BS-Eg@users.noreply.github.com>
2023-03-27 22:52:26 +0200

    Update bseg.sh

--- bseg.sh
+++ bseg.sh
@@ -2,9 +2,9 @@
 apt install git -y
 cd ..
 mkdir -p odoo      
-mkdir -p odoo/141      
 mkdir -p odoo/151      
-mkdir -p odoo/161
+mkdir -p odoo/152
+mkdir -p odoo/153
 git clone  https://github.com/BS-Eg/DockerBS
 cd /DockerBS
 chmod +x  runme.sh
