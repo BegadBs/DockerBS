1d9908b6ac05d7a75e97ac8a9b0f4edff760719e
BS-Eg <120326341+BS-Eg@users.noreply.github.com>
2023-03-27 22:57:59 +0200

    Update bseg.sh

--- bseg.sh
+++ bseg.sh
@@ -4,8 +4,9 @@ cd ..
 mkdir -p odoo      
 mkdir -p odoo/151      
 mkdir -p odoo/152
-mkdir -p odoo/npm
 git clone  https://github.com/BS-Eg/DockerBS
 cd /DockerBS
+cp -r -f runme.sh /odoo
+cd /odoo
 chmod +x  runme.sh
 ./runme.sh
